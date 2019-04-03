c6557380b6707649f8bd74ea2d63baf5f3f9e1b5
Ricky Lu <real0st@gmail.com>
2019-04-03 22:54:13 +0100

    add view-stream functionality

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -68,4 +68,5 @@ export const deleteStream = (id) => async dispatch => {
 		type: DELETE_STREAM,
 		payload: id
 	});
+	history.push('/');
 }
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -13,10 +13,14 @@ class StreamList extends React.Component {
 		if (stream.userId === this.props.currentUserId) {
 			return (
 				<div className='right floated content'>
-					<Link to={`/streams/edit/${stream.id}`} className='ui button primary'>
+					<Link to={`/streams/edit/${stream.id}`} 
+						className='ui button primary'
+					>
 						Edit
 					</Link>
-					<Link to={`/streams/delete/${stream.id}`} className='ui button negative'>
+					<Link to={`/streams/delete/${stream.id}`}
+						className='ui button negative'
+					>
 						Delete
 					</Link>
 				</div>
@@ -31,7 +35,9 @@ class StreamList extends React.Component {
 					{ this.renderAdmin(stream) }
 					<i className="large middle aligned icon camera" />
 					<div className='content'>
-						{stream.title}
+						<Link to={`/streams/${stream.id}`} >
+							{stream.title}
+						</Link>
 						<div className="description">
 							{ stream.description }
 						</div>
